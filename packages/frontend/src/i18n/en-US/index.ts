efc39aa6c9ead69d5d7c111ce030819c17c14fa9
huaiyuan <1029848564@qq.com>
2024-11-08 19:11:23 +0800

    feat: update LarePass

--- packages/frontend/src/i18n/en-US/index.ts
+++ packages/frontend/src/i18n/en-US/index.ts
@@ -1,8 +1,8 @@
 export default {
 	login_title: 'Enter password to log in',
 	login_hint_password: 'Password',
-	mobile_veri: 'Confirm on TermiPass app',
-	login_useing_auth: 'Veify using one-time password from TermiPass',
+	mobile_veri: 'Confirm on LarePass app',
+	login_useing_auth: 'Veify using one-time password from LarePass',
 	totp_title: 'Safety verification',
-	totp_message: 'Enter the one-time password from TermiPass'
+	totp_message: 'Enter the one-time password from LarePass'
 };
--- packages/frontend/src/i18n/zh-CN/index.ts
+++ packages/frontend/src/i18n/zh-CN/index.ts
@@ -1,8 +1,8 @@
 export default {
 	login_title: '输入密码登录',
 	login_hint_password: '密码',
-	mobile_veri: '在 TermiPass 应用程序上确认',
-	login_useing_auth: '使用 TermiPass 生成的一次性密码验证',
+	mobile_veri: '在 LarePass 应用程序上确认',
+	login_useing_auth: '使用 LarePass 生成的一次性密码验证',
 	totp_title: '安全验证',
-	totp_message: '输入 TermiPass 的一次性密码'
+	totp_message: '输入 LarePass 的一次性密码'
 };
